365f8b761c1017118a1c9a8d20b88740cd665711
Nandhinioff <155603422+Nandhinioff@users.noreply.github.com>
2024-06-15 23:32:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let box = document.getElementById("cards");
+let API = async () => {
+    let response = await fetch("https://reqres.in/api/users?page=1");
+    let file = await response.json();
+    let info = file.data.map((element) => {
+        return `<div class="card">
+                    <div class="profile-image">
+                    <img src="${element.avatar}" alt="">
+                    </div>
+                    <div class="card-content">
+                    <h3>${element.first_name} ${element.last_name}</h3>
+                    <h2>${element.email}</h2>
+                    <h2>User ID: ${element.id}</h2>
+                    </div>
+                </div>`;
+    }).join("")
+    cards.innerHTML = info;
+};
+let navBtn = document.getElementById("push");
+navBtn.addEventListener("click", () => {
+    cards.innerHTML = `<div class="load">
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+  </div>`;
+    setTimeout(() => {
+        API();
+    }, 10000);
+});
